cb1ffd3bade0788e50de274d56c4a6fc98588617
kenara <4996484+kenara@users.noreply.github.com>
2021-03-31 12:25:41 +0100

    Update initFirebase.js

--- src/components/initFirebase.js
+++ src/components/initFirebase.js
@@ -4,12 +4,17 @@ import 'firebase/database'; // If using Firebase database
 import 'firebase/storage';  // If using Firebase storage
 
 var config = {
-	apiKey: "AIzaSyCccvko6m2qAlRgNeoICbeM9CjwQSNMKZQ",
-	authDomain: "kirus-craigslist.firebaseapp.com",
-	databaseURL: "https://kirus-craigslist.firebaseio.com",
-	projectId: "kirus-craigslist",
-	storageBucket: "kirus-craigslist.appspot.com",
-	messagingSenderId: "545866356812"
+
+  apiKey: "AIzaSyDktgQND4Vg_rSftOsNvK0LOe0sL9H4yrY",
+  authDomain: "unjobs-post.firebaseapp.com",
+  databaseURL: "https://unjobs-post.firebaseio.com",
+  projectId: "unjobs-post",
+  storageBucket: "unjobs-post.appspot.com",
+  messagingSenderId: "654532906512",
+  appId: "1:654532906512:web:8de7b7ea721243667fc495",
+  measurementId: "G-YXE9PDKKNS"
+	
+	
 };
 if (!firebase.apps.length) {
 	firebase.initializeApp(config);
